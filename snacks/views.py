301b93d211c0cf524a2ebf9a00f67f5f47f956b0
Joe Streifel <josephstreifel@gmail.com>
2022-01-09 17:13:44 -0800

    views, urls
    sets up required views and urls

--- snacks/views.py
+++ snacks/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView, DeleteView, DetailView, CreateView, UpdateView
+
+from snacks.models import Snack
+
 
 # Create your views here.
+
+
+class SnackListView(TemplateView):
+    template_name = 'snacks_list.html'
+    model = Snack
+
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create.html'
+    model = Snack
+
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update.html'
+    model = Snack
+
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete.html'
+    model = Snack
+
